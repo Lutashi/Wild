d29e7d3e80d34a08551f574d99bb1cd3e149bd70
Lutashi <ajumashukurov@gmail.com>
2023-03-01 17:12:50 +0600

    init5

---
+++ script.js
@@ -0,0 +1,29 @@
+let carouselSlide = document.querySelector(".slideshow")
+let carouselImages = document.querySelectorAll(".slideshow img")
+
+let prevButton = document.querySelector("#prevBtn")
+let nextButton = document.querySelector("#nextBtn")
+
+let counter = 0
+let size = carouselImages[0].clientWidth
+
+// carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+
+nextButton.addEventListener('click',()=>{
+    if (counter >= 1){
+        counter = -1
+    }
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter ++
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+})
+
+prevButton.addEventListener('click',()=>{
+    if (counter <= 0){
+        counter = 2
+    }
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter --
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+})
+
